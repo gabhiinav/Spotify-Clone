183b5eaf4caca320dce5d4f6cac688985f34c7c5
gabhiinav <gabhiinav@gmail.com>
2023-06-07 14:03:09 +0530

    Settings

---
+++ app/(site)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="text-blue-700 flex min-h-screen flex-col items-center justify-between p-24">
+      Hello World!
+    </div>
+  )
+}
