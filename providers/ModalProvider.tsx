1172365890a32e913c048d57df546424cecaaa55
gabhiinav <gabhiinav@gmail.com>
2023-06-15 01:05:42 +0530

    All Modals

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,11 +1,34 @@
-"use client"
+"use client";
 
-import React from 'react'
+import { useEffect, useState } from "react";
 
-const ModalProvider = () => {
-  return (
-    <div>ModalProvider</div>
-  )
+import AuthModal from "@/components/AuthModal";
+import SubscribeModal from "@/components/SubscribeModal";
+import UploadModal from "@/components/UploadModal";
+import { ProductWithPrice } from "@/types/types";
+
+interface ModalProviderProps {
+  products: ProductWithPrice[];
 }
 
-export default ModalProvider
+const ModalProvider: React.FC<ModalProviderProps> = ({ products }) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <AuthModal />
+      <SubscribeModal products={products} />
+      <UploadModal />
+    </>
+  );
+};
+
+export default ModalProvider;
