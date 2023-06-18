b3899ca92d4c715378948376d720a7fd46f43fcf
gabhiinav <gabhiinav@gmail.com>
2023-06-18 17:11:09 +0530

    Front text

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -29,7 +29,7 @@ export default async function Home() {
               font-semibold
             "
           >
-            Welcome back
+            Welcome back.
           </h1>
           <div
             className="
@@ -52,7 +52,7 @@ export default async function Home() {
       </Header>
       <div className="mt-2 mb-7 px-6">
         <div className="flex justify-between items-center">
-          <h1 className="text-white text-2xl font-semibold">Newest songs</h1>
+          <h1 className="text-white text-2xl font-semibold">Songs</h1>
         </div>
         <PageContent songs={songs} />
       </div>
